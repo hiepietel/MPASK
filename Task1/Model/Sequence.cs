efdb6c981955f5fbd958f72f7bc869c9fb52b5ae
jachumail <jachumail@yahoo.pl>
2019-11-24 22:50:27 +0100

    Sequence init

---
+++ Task1/Model/Sequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Model
+{
+    public class Sequence
+    {
+        public string name { get; set; }
+        public bool IsSequenceOf { get; set; }
+        public List<ElementOfSequnce> ElementsOfSequnces { get; set; }
+    }
+
+    public class ElementOfSequnce
+    {
+        public string name { get; set; }
+        public DataType Data { get; set; }
+        public string Restrictions { get; set; }
+    }
+}
---
+++ Task1/Parser/SequenceParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Parser
+{
+    public static class SequenceParser
+    {
+    //    public static List<DataType> ReturnTree(string filepath, List<DataType> dataTypes)
+    //    {
+    //        MatchCollection matchesData = TaskMethods.CollectionRegex("data/" + filepath.ReturnFilePath(), RegexString.DataTypeRGX);
+    //        // MatchCollection matchesData = TaskMethods.CollectionRegex("data/" + mainFilePath.ReturnFilePath(), RegexString.DataTypeRGX);
+    //        List<DataType> list = DoTree(matchesData, dataTypes);
+    //        return dataTypes;
+    //    }
+    //
+    }
+}
