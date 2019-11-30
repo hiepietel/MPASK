a1808c468ff3563d7974ec1653b2d061c978f7fb
jachumail <jachumail@yahoo.pl>
2019-11-30 13:45:44 +0100

    Move Main to class

---
+++ Task1/MIBreader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Task1;
+using Task1.Method;
+using Task1.Model;
+using Task1.Parser;
+
+
+namespace Task1
+{
+    public partial class MIBreader
+    {
+        public LeafNode leafs { get; set; }
+        public List<string> importedFiles { get; set; }
+        public List<DataType> dataTypes { get; set; }
+        string MainFilePath { get; set; }
+        public MIBreader()
+        {
+            MainFilePath = "RFC1213";
+            Init();
+        }
+        public MIBreader(string MFP)
+        {
+            MainFilePath = MFP;
+            Init();
+        }
+        public void Import()
+        {
+            Import(MainFilePath);
+            ReturnAllTree(MainFilePath);
+        }
+        partial void Import(string filePath);
+        partial void Init();
+        partial void ReturnAllTree(string from);
+    }
+    partial class MIBreader
+    {
+        partial void Init()
+        {
+            leafs = LeafParser.InitNodeTRee();
+            importedFiles = new List<string>();
+            dataTypes = new List<DataType>();
+
+        }
+        partial void ReturnAllTree(string from)
+        {
+            importedFiles.Add(from);
+            dataTypes = DataTypeParser.ReturnTree(from, dataTypes);
+            leafs = LeafParser.ReturnTree(from, leafs);
+            leafs = LeafDataParser.ReturnTree(from, leafs, dataTypes);
+
+        }
+        partial void Import(string filePath)
+        {
+
+            Match imports = TaskMethods.MatchRegex("data/" + filePath.ReturnFilePath(), RegexString.ImportsRGX);
+            string toImport = imports.Value.Replace("IMPORTS", "").RemoveSpecialCharacter().RemoveSpaces();
+            MatchCollection importData = TaskMethods.CollectionRegex(toImport, RegexString.ImportSortedRGX, false);
+            foreach (Match itemm in importData)
+            {
+                //List<string> datatypesToImport= itemm.Groups[1].Value.RemoveSpecialCharacter().Split(',').T;
+                string[] items = itemm.Groups[1].Value.RemoveSpecialCharacter().Split(',');
+                string from = itemm.Groups[2].Value.RemoveSpecialCharacter();
+
+
+                if (!importedFiles.Contains(from))
+                {
+                    ReturnAllTree(from);
+                    Import(from);
+                }
+
+            }
+        }
+    }
+
+
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,60 +9,12 @@ using Task1.Parser;
 
 class Program
 {
-    static LeafNode leafs;
-    static List<string> importedFiles = new List<string>();
-    static List<DataType> dataTypes = new List<DataType>();
-    static void Import(string mainFilePath)
-    {
-        Match imports = TaskMethods.MatchRegex("data/" + mainFilePath.ReturnFilePath(), RegexString.ImportsRGX);
-        string toImport = imports.Value.Replace("IMPORTS", "").RemoveSpecialCharacter().RemoveSpaces();
-        MatchCollection importData = TaskMethods.CollectionRegex(toImport, RegexString.ImportSortedRGX, false);
-        foreach (Match itemm in importData)
-        {
-            //List<string> datatypesToImport= itemm.Groups[1].Value.RemoveSpecialCharacter().Split(',').T;
-            string[] items = itemm.Groups[1].Value.RemoveSpecialCharacter().Split(',');
-            string from = itemm.Groups[2].Value.RemoveSpecialCharacter();
-            
-            
-            if (!importedFiles.Contains(from))
-            {
-                importedFiles.Add(from);
-                leafs = LeafParser.ReturnTree(from, leafs);
-                dataTypes = DataTypeParser.ReturnTree(from, dataTypes);
-                Import(from);
-            }
-            
-        }
-    }
     static void Main()
     {
-        ////initData
-        string mainFilePath = "RFC1213";
-        //leafs = LeafParser.InitTree();
-        leafs = LeafParser.InitNodeTRee();
-        ////ImportFromFiles
-        Import(mainFilePath);
-        ////Add main file 
-        importedFiles.Add("data/"+mainFilePath);
-        
-        leafs = LeafParser.ReturnTree(mainFilePath, leafs);
-        dataTypes = DataTypeParser.ReturnTree(mainFilePath, dataTypes);
-        //MatchCollection matches = TaskMethods.CollectionRegex("data/" + mainFilePath.ReturnFilePath(), RegexString.LeafDataRGX);
-        leafs = LeafDataParser.ReturnTree(mainFilePath, leafs, dataTypes);
-
-        leafs.PrintTree(leafs);
-        LeafNode? searched = leafs.SearchByOID("1.3.6.1",leafs);
-        mainFilePath = "RFC1213";
-        
-        //MatchCollection matchesData = Regex.Matches(TaskMethods.ReadFile("data/FC1155SMI.txt"), dataTypeRGXverOne, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
-        //MatchCollection matchesData = TaskMethods.CollectionRegex("data/" + mainFilePath.ReturnFilePath(), RegexString.DataTypeRGX);
-        //var dataTypes = DataTypeParser.DoTree(matchesData);
-
-
-        //MatchCollection matches = Regex.Matches(TaskMethods.ReadFile("data/MIB.txt"), rgxPro, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
-
-
-        //List <LeafData> listOfLeafs = new List<LeafData>();
+        MIBreader mibreader = new MIBreader();
+        mibreader.Import();
+        mibreader.leafs.PrintTree(mibreader.leafs);
+        LeafNode? searched = mibreader.leafs.SearchByOID("1.3.6.1", mibreader.leafs);
         Console.ReadKey();
     }
 
