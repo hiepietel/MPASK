0eb891f449ac1b410f67cbeaa28d155aeb4d4c8b
jachumail <jachumail@yahoo.pl>
2020-01-22 23:42:51 +0100

    Converter to ENUM, not finished

--- Task3.Tests/ConverterToEnumTests.cs
+++ Task3.Tests/ConverterToEnumTests.cs
@@ -11,8 +11,13 @@ namespace Task3.ConverterToEnumTests
         [TestMethod]
         public void ConvertToTimeticksTest()
         {
+            
             DataType dataType = ConverterToEnum.ToSimpleDatatype("43");
             Assert.AreEqual(dataType, DataType.Timeticks);
+            dataType = ConverterToEnum.ToSimpleDatatype("01");
+            Assert.AreEqual(dataType, DataType.BOOLEAN);
+            dataType = ConverterToEnum.ToSimpleDatatype("02");
+            Assert.AreEqual(dataType, DataType.INTEGER);
         }
     }
 }
---
+++ Task3/Method/ConverterToOID.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Method
+{
+    public class ConverterToOID
+    {
+        public string ToOID(string hexOID)
+        {
+            //TODO first two oid
+            
+            string OID = "";
+            for(int i=2; i<hexOID.Length; i += 2)
+            {
+                string temp = hexOID.ElementAt(i).ToString()+ hexOID.ElementAt(i+1).ToString();
+                int num = int.Parse(temp, System.Globalization.NumberStyles.HexNumber);
+                OID += num.ToString() + ".";
+            }
+            OID = OID.Remove(OID.LastIndexOf('.'), 1);
+            return OID;
+        }
+    }
+}
